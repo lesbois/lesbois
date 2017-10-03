56c26dd6b28c03866a11574d7dfe001537c5f717
Kin Basco <kinbasco@Kins-iMac.local>
2017-10-03 14:13:36 +0800

    Setup sass and spectre

--- client/config/base.js
+++ client/config/base.js
@@ -22,6 +22,15 @@ export const rules = [
         test: /\.js$/,
         exclude: /node_modules/,
         loader: "babel-loader"
+    },
+    {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+            {loader: 'style-loader'},
+            {loader: 'css-loader'},
+            {loader: 'sass-loader'}
+        ]
     }
 ]
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import { render } from 'react-dom'
 import React from 'react'
 import ClientProvider from './providers/ClientProvider'
+import './assets/app.scss'
 
 
 const useClient = (props, target) => {
