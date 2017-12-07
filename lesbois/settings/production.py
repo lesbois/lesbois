dffe4039e4e7b99207a83915173cb95c05a1e8cb
Kin Basco <freepeace13@gmail.com>
2017-12-07 16:09:34 +0800

    Remove database host and port variable

--- lesbois/settings/production.py
+++ lesbois/settings/production.py
@@ -14,8 +14,6 @@ DATABASE = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': env.str('DATABASE_NAME'),
         'USER': env.str('DATABASE_USER'),
-        'PASSWORD': env.str('DATABASE_PASSWORD'),
-        'HOST': env.str('DATABASE_HOST'),
-        'PORT': env.str('DATABASE_PORT')
+        'PASSWORD': env.str('DATABASE_PASSWORD')
     }
 }
